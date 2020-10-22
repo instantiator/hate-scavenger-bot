5ea1e98f9e2f036f92eb126fd952b57aeb3f76a5
Chris <elysuim_1@hotmail.com>
2020-10-22 18:53:19 +0100

    Update helperMethods.py

--- helperMethods.py
+++ helperMethods.py
@@ -39,10 +39,6 @@ def cleanup_data(data:Dict[str, Any]) -> Dict[str, Any]:
         data['text'] = data['full_text']
     data.pop('full_text')
 
-    # Remove Identifying info from text fields
-    data['text'] = remove_user_info_from_tweet(data['text'])
-    data['explanatory_text'] = remove_user_info_from_tweet(data.get('explanatory_text'))
-
     # Convert hashtag list to 'csv' style string
     data['hashtags'] = ','.join(data['hashtags'])
 
