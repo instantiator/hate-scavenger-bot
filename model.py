c394e62d229fe748866dcff6e47b670deda48362
Chris <elysuim_1@hotmail.com>
2020-10-22 15:02:13 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,6 +1,12 @@
 import tweepy
+import logging
+import os
+import json
+from datetime import datetime
+from typing import List
 
 from auth import API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, BEARER_TOKEN
+import helperMethods
 
 
 def authenticate():
@@ -14,12 +20,70 @@ def authenticate():
     api = tweepy.API(auth)
     return api
 
+def get_mentions(api) -> List:
+    return api.mentions_timeline()
+
+
+def process_quote(api, tweet) -> str:
+    explanatory_text = tweet.text
+    quoted_message_id = tweet.quoted_status_id
+    target_tweet = api.get_status(quoted_message_id)
+    
+    json_tweet = target_tweet._json
+    json_tweet.update({"explantory_text": explanatory_text})
+    return json.dumps(json_tweet)
+
+
+def process_reply(api, tweet) -> str:
+    explanatory_text = tweet.text
+    reply_id: int = tweet.in_reply_to_status_id
+    target_tweet = api.get_status(reply_id)
+
+    json_tweet = target_tweet._json
+    json_tweet.update({"explantory_text": explanatory_text})
+    return json.dumps(json_tweet)
+
+
+def process_mention(tweet) -> str:
+    json_tweet = tweet._json
+    return json.dumps(json_tweet)
+
+
 
 if __name__ == '__main__':
 
-    api = authenticate()
-    
-    # Create a tweet
-    api.update_status("Hello World!")
+    OUTPUT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "output")
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
 
+    log_filepath = os.path.join(OUTPUT_DIR, "model.log")
+    logging.basicConfig(filename=log_filepath, level=logging.INFO) # DEBUG level also seems to get information from tweepy itself.
+
+    logging.info(f"Running application at: {datetime.now()}")
+
+    api = authenticate()
+    my_name = api.me().screen_name
+    logging.info("Authenticating as ({}): {}".format(my_name, "SUCCESS" if api is not None else "FAILURE"))
+    
+    recent_mentions = get_mentions(api)
+    logging.info(f"Getting Mentions: {len(recent_mentions)} found")
+
+    for mention in recent_mentions:
+        logging.info("Processing Tweet: {mention.id} sent from user: {mention.user} ...")
+
+        if mention.is_quote_status:
+            logging.info("Tweet {mention.id} is being processed as type 'quote'.")
+            json_tweet = process_quote(api, mention)
+        
+        elif isinstance(mention.in_reply_to_status_id, int): 
+            logging.info("Tweet {mention.id} is being processed as type 'reply'.")
+            json_tweet = process_reply(api, mention)
+            
+        else:
+            logging.info("Tweet {mention.id} is being processed as type 'other'.")
+            json_tweet = process_mention(mention)
+
+        
+
+    logging.info("======================================\n")
     print("done")
