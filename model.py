49aef8000f10233b4b4556d4c85f373a0ed9ab69
Chris <elysuim_1@hotmail.com>
2020-10-23 16:56:44 +0100

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import logging
 import os
 import json
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from auth import API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, BEARER_TOKEN
 from helperMethods import parse_tweet_data, cleanup_data
@@ -20,8 +20,11 @@ def authenticate():
     api = tweepy.API(auth)
     return api
 
-def get_mentions(api) -> List:
-    return api.mentions_timeline(count=100)
+def get_mentions(api, since:int=None) -> List:
+    if since is not None:
+        return api.mentions_timeline(since_id=since,count=999)
+
+    return api.mentions_timeline(count=999)
 
 
 def process_quote(api, tweet) -> str:
@@ -49,6 +52,30 @@ def process_mention(tweet) -> str:
     return json.dumps(json_tweet)
 
 
+def get_most_recent_tweet_id_from_database(file:str) -> Optional[int]:
+    """
+    Figure out the last tweet we added to the database, 
+    return the id of that.
+
+    Returns None for empty database
+    """
+
+    ## As a tmp measure, we are saving/reading this value from a file.
+    if not os.path.exists(file):
+        return None
+    
+    tweet_id = None
+    with open(file, "r") as f:
+        tweet_id = int(f.readline())
+
+    return tweet_id
+
+def set_most_recent_tweet_id_in_database(file:str, tweet_id:int):
+    ## As a tmp measure, we are saving/reading this value from a file.
+    with open(file, "w") as f:
+        f.write(str(tweet_id))
+
+
 def add_to_database(tweet, filepath):
     with open(filepath,"a") as f:
         f.write(json.dumps(tweet))
@@ -69,9 +96,17 @@ if __name__ == '__main__':
     api = authenticate()
     my_name = api.me().screen_name
     logging.info("Authenticating as ({}): {}".format(my_name, "SUCCESS" if api is not None else "FAILURE"))
-    
-    recent_mentions = get_mentions(api)
-    logging.info(f"Getting Mentions: {len(recent_mentions)} found")
+
+    ## Only attempt to process mentions we have not previously added to DB
+    database_last_tweet_filename = os.path.join(OUTPUT_DIR, "last_tweet_id.txt")
+    last_tweet_added_id = get_most_recent_tweet_id_from_database(database_last_tweet_filename)
+    if last_tweet_added_id is not None:
+        logging.info(f"Last Tweet added to database was {last_tweet_added_id}. Getting all mentions since then...")
+        recent_mentions = get_mentions(api, last_tweet_added_id) 
+    else:
+        recent_mentions = get_mentions(api)
+
+    logging.info(f"{len(recent_mentions)} Mentions found")
 
     for mention in recent_mentions:
         logging.info(f"Processing Tweet: {mention.id} sent from user: {mention.user.screen_name} ...")
@@ -97,6 +132,9 @@ if __name__ == '__main__':
         database_filename = os.path.join(OUTPUT_DIR, "database.txt")
         add_to_database(cleaned_data, database_filename)
 
+    logging.info(f"Setting 'Last_tweet_id' to value: {recent_mentions[0].id}")
+    set_most_recent_tweet_id_in_database(database_last_tweet_filename, recent_mentions[0].id)
 
+    logging.info(f"All processing completed at: {datetime.now()}")
     logging.info("======================================\n")
     exit(0)
