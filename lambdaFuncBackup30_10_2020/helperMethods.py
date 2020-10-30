86abd2a44d2f75de50c461abbaf9981503a480a0
Chris <elysuim_1@hotmail.com>
2020-10-30 17:52:42 +0000

    add backup

---
+++ lambdaFuncBackup30_10_2020/helperMethods.py
@@ -0,0 +1,43 @@
+from typing import Dict, Any
+import json
+
+TWEET_FIELDS = ['id', 'created_at', 'full_text', 'text', ('user', 'screen_name'), 'lang', 'retweet_count', 'favorite_count', 'geo',
+                'notify_text', 'notify_tweet_id', 'notify_is_reply', 'notify_is_retweet', 'notify_screen_name']
+                
+# Other fields in DB:  'entry_added_by', "uid"
+
+def parse_tweet_data(json_string: str, fields=TWEET_FIELDS) -> Dict[str, Any]:
+    '''
+    Takes a json string and extracts the fields we care about.
+    '''
+
+    raw_data = json.loads(json_string)
+
+    filtered_data = dict()
+    for f in fields:
+        if isinstance(f, str):
+            value = raw_data.get(f)
+            filtered_data[f] = value
+
+        elif isinstance(f, tuple):
+            # If field is tuple, then data is nested.
+            # for instance (x, y, z) => dict[x][y][z]
+            # They maybe a more elegant way todo this. :)
+            tmp = raw_data
+            for sub_field in f:
+                if isinstance(tmp, dict):
+                    tmp = tmp.get(sub_field)
+
+            key = f[-1]
+            filtered_data[key] = tmp
+
+    return filtered_data
+
+
+def cleanup_data(data:Dict[str, Any]) -> Dict[str, Any]:
+    # Remove 'full_text' key, add content to 'text' key
+    if data['full_text'] is not None:
+        data['text'] = data['full_text']
+    data.pop('full_text')
+
+    return data
